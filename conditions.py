19fabe67ddf73d5f425c1114d9a29706addf19df
Elain Anne C. Santos <elac.santos.au@phinmaed.com>
2026-09-03 10:59:02 +0800

    Update conditions.py

--- conditions.py
+++ conditions.py
@@ -1,1 +1,24 @@
+age = int(input("Enter your age: "))
+print()
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
+print("-------------------------")
+score = int(input("Enter your score: "))
+print()
+if score >= 90:
+    print("Excellent")
+elif score >=75:
+    print("Good")
+else:
+    print("Needs Improvement")
+
+print("________________________")
+
+number = 10
+
+if number == 10:
+ print("Ten")
+
 
